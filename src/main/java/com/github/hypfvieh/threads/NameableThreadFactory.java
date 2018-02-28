c0b675a912bfc002ee63d2760a1329e748a35387
David M <maniac@case-of.org>
2018-02-28 07:11:51 +0100

    Added thread factory for use in ExecutorService which allows setting the thread names and the daemon status of the threads

---
+++ src/main/java/com/github/hypfvieh/threads/NameableThreadFactory.java
@@ -0,0 +1,45 @@
+package com.github.hypfvieh.threads;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.github.hypfvieh.util.StringUtil;
+
+public class NameableThreadFactory implements ThreadFactory {
+
+    private static final AtomicInteger POOL_NUMBER = new AtomicInteger(1);               
+    private final ThreadGroup          group;
+    private final AtomicInteger        threadNumber = new AtomicInteger(1);
+    private final String               namePrefix;
+
+    private final boolean              daemonizeThreads;
+    
+    /**
+     * Create a new ThreadFactory instance.
+     * The thread name is created like this:
+     * _name + THREAD_NUMBER
+     * e.g: connectionPool-1
+     * If _name is null or blank, UnnamedThreadPool-POOL_NUMBER-thread-THREAD_NUMBER will be used.
+     * 
+     * @param _name prefix for all thread names
+     * @param _threadPrefix suffix for all thread names
+     * @param _daemonizeThreads turn all created threads to daemon threads
+     */
+    public NameableThreadFactory(String _name, boolean _daemonizeThreads) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        namePrefix = StringUtil.isBlank(_name) ? "UnnamedThreadPool-" + POOL_NUMBER.getAndIncrement() + "-thread-" : _name;
+        daemonizeThreads = _daemonizeThreads;
+    }
+
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+        t.setDaemon(daemonizeThreads);
+        
+        if (t.getPriority() != Thread.NORM_PRIORITY) {
+            t.setPriority(Thread.NORM_PRIORITY);
+        }
+        return t;
+    }
+
+}
