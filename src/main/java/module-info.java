04b7b8d1438192d5c76e8298e49f59c2628de5be
David M <maniac@case-of.org>
2021-11-18 13:44:49 +0100

    JPMS

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,20 @@ module com.github.hypfvieh.java.utils {
     opens com.github.hypfvieh.db;
     opens com.github.hypfvieh.config.xml;
 
+    exports com.github.hypfvieh.collections;
+    exports com.github.hypfvieh.system;
+    exports com.github.hypfvieh.files;
+    exports com.github.hypfvieh.util;
+    exports com.github.hypfvieh.stream;
+    exports com.github.hypfvieh.util.xml;
+    exports com.github.hypfvieh.threads;
+    exports com.github.hypfvieh.formatter;
+    exports com.github.hypfvieh.imaging;
+    exports com.github.hypfvieh.common;
+    exports com.github.hypfvieh.classloader;
+    exports com.github.hypfvieh.db;
+    exports com.github.hypfvieh.config.xml;
+
     requires java.desktop;
     requires java.management;
     requires transitive java.sql;
