28207fb5a857b242bb1fb42ec60555eb49f759c7
David M <maniac@case-of.org>
2021-11-18 08:23:13 +0100

    Module-info: export vs opens

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,17 @@
 module com.github.hypfvieh.java.utils {
-    exports com.github.hypfvieh.collections;
-    exports com.github.hypfvieh.system;
-    exports com.github.hypfvieh.files;
-    exports com.github.hypfvieh.util;
-    exports com.github.hypfvieh.stream;
-    exports com.github.hypfvieh.util.xml;
-    exports com.github.hypfvieh.threads;
-    exports com.github.hypfvieh.formatter;
-    exports com.github.hypfvieh.imaging;
-    exports com.github.hypfvieh.common;
-    exports com.github.hypfvieh.classloader;
-    exports com.github.hypfvieh.db;
-    exports com.github.hypfvieh.config.xml;
+    opens com.github.hypfvieh.collections;
+    opens com.github.hypfvieh.system;
+    opens com.github.hypfvieh.files;
+    opens com.github.hypfvieh.util;
+    opens com.github.hypfvieh.stream;
+    opens com.github.hypfvieh.util.xml;
+    opens com.github.hypfvieh.threads;
+    opens com.github.hypfvieh.formatter;
+    opens com.github.hypfvieh.imaging;
+    opens com.github.hypfvieh.common;
+    opens com.github.hypfvieh.classloader;
+    opens com.github.hypfvieh.db;
+    opens com.github.hypfvieh.config.xml;
 
     requires java.desktop;
     requires java.management;
