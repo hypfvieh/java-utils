612e7f10d0cbceab01645ecc7c88e44bae4754d7
David M <maniac@case-of.org>
2023-01-12 15:50:34 +0100

    Updated TimeMeasure to stop usage of System.currentMillis

--- src/main/java/com/github/hypfvieh/util/TimeMeasure.java
+++ src/main/java/com/github/hypfvieh/util/TimeMeasure.java
@@ -1,127 +1,148 @@
 package com.github.hypfvieh.util;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import java.time.Duration;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Utility class for time measurements.
+ * Utility class for time measurements.<br>
  * Instances may be reset for reuse.
  */
-public class TimeMeasure {
+public final class TimeMeasure {
 
-    public static interface ITimeMeasureFormat {
-        public String format(long _durationInMillis);
-    }
+    private static final long MILLIS_TO_NANOS = TimeUnit.MILLISECONDS.toNanos(1);
 
-    /** Start time in milliseconds. */
-    private volatile long startTm;
-
-    /** Formatter used for {@link #toString()} */
-    private final ITimeMeasureFormat tmf;
+    /** Start time in nanoseconds. */
+    private volatile long     startTime;
 
     /**
-     * Create a new instance using _ts millis as
-     * @param _formatter formatter to use for toString() call
+     * Default constructor with start time equals current time.
      */
-    public TimeMeasure(ITimeMeasureFormat _formatter) {
-        tmf = _formatter;
+    public TimeMeasure() {
         reset();
     }
 
     /**
-     * Create a new instance, used a formatter converting everything &gt;= 5000 ms to seconds (X.Y -&gt; 6.1).
+     * Constructor with a start time (useful for unit testing).
+     * @param _startTime start time in nanoseconds
      */
-    public TimeMeasure() {
-        this(new ITimeMeasureFormat() {
-            @Override
-            public String format(long _durationInMillis) {
-                return _durationInMillis >= 5000 ? ((long) ((_durationInMillis / 1000d) * 10) / 10d) + "s" : _durationInMillis + "ms";
-            }
-        });
+    TimeMeasure(long _startTime) {
+        startTime = _startTime;
     }
 
     /**
-     * Resets the start time to current time in milliseconds.
+     * Resets the start time to current time.
+     *
      * @return the object
      */
-    public final TimeMeasure reset() {
-        this.startTm = System.currentTimeMillis();
+    public TimeMeasure reset() {
+        startTime = System.nanoTime();
         return this;
     }
 
     /**
-     * Returns the start time in milliseconds.
-     * @return start time in ms
+     * Gets the elapsed time in milliseconds.
+     *
+     * @return elapsed time in milliseconds
      */
-    public long getStartTime() {
-        return startTm;
+    public long getElapsedMillis() {
+        return getElapsedNanos() / MILLIS_TO_NANOS;
     }
 
     /**
-     * Returns the elapsed time in milliseconds.
-     * @return elapsed time in ms
+     * Gets the elapsed time in nanoseconds.
+     *
+     * @return elapsed time in nanoseconds
      */
-    public long getElapsed() {
-        return System.currentTimeMillis() - startTm;
+    public long getElapsedNanos() {
+        return System.nanoTime() - startTime;
     }
 
     /**
-     * Formats the elapsed time using the given dateFormatter.
-     * If null is given, a new Formatter with format HH:mm:ss.SSS will be used.
+     * Gets the elapsed time in seconds.
      *
-     * The timezone of the given dateFormatter will always be set to 'UTC' to avoid any timezone related offsets.
+     * @return elapsed time in seconds
+     */
+    public long getElapsedSeconds() {
+        return TimeUnit.NANOSECONDS.toSeconds(getElapsedNanos());
+    }
+
+    /**
+     * Gets the elapsed time in fractional minutes.
      *
-     * @param _dateFormat date format
+     * @return elapsed time in minutes
+     */
+    public double getElapsedMinutes() {
+        return TimeUnit.NANOSECONDS.toSeconds(getElapsedNanos()) / 60d;
+    }
+
+    /**
+     * Returns the elapsed time as a formatted, user-friendly string.
+     *
      * @return formatted string
      */
-    public String getElapsedFormatted(DateFormat _dateFormat) {
-        return getElapsedFormatted(_dateFormat, getElapsed());
+    public String getElapsedFormatted() {
+        return formatDuration((double) getElapsedNanos() / MILLIS_TO_NANOS);
     }
 
     /**
-     * Same as above, used for proper unit testing.
-     * @param _dateFormat
-     * @param _elapsedTime
-     * @return formatted string
+     * Gets the elapsed time in milliseconds and resets the instance.
+     *
+     * @return elapsed time
      */
-    String getElapsedFormatted(DateFormat _dateFormat, long _elapsedTime) {
-        Date elapsedTime = new Date(_elapsedTime);
-
-        DateFormat sdf = _dateFormat;
-        if (_dateFormat == null) {
-            sdf = new SimpleDateFormat("HH:mm:ss.SSS");
-        }
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC")); // always use UTC so we get the correct time without timezone offset on it
-
-        return sdf.format(elapsedTime);
-    }
-
-    /**
-     * Change the start time (for unit testing only!).
-     * @param _tm
-     */
-    void setStartTm(long _tm) {
-        startTm = _tm;
-    }
-
     public long getElapsedAndReset() {
-        long elapsed = getElapsed();
+        long elapsed = getElapsedMillis();
         reset();
         return elapsed;
     }
 
     /**
-     * Returns the elapsed time in milliseconds formatted as string.
-     * @return elapsed time in ms
+     * Converts a fractional milliseconds duration to a user-friendly formatted string e.g. for logging.
+     *
+     * @param _millis elapsed time in milliseconds
+     * @return formatted string
+     */
+    public static String formatDuration(double _millis) {
+        return formatDuration(_millis, Locale.getDefault());
+    }
+
+    /**
+     * Converts a fractional milliseconds duration to a user-friendly formatted string e.g. for logging.
+     *
+     * @param _millis elapsed time in milliseconds
+     * @param _locale locale
+     * @return formatted string
+     */
+    static String formatDuration(double _millis, Locale _locale) {
+        if (_millis < 1000) {
+            return String.format(_locale, "%.2f ms", _millis);
+        }
+        if (_millis < 10000) {
+            return String.format(_locale, "%d ms", (long) _millis);
+        }
+
+        long seconds = (long) (_millis / 1000);
+        if (seconds < 300) {
+            return seconds + " seconds";
+        }
+        long minutes = TimeUnit.SECONDS.toMinutes(seconds);
+        if (minutes < 60) {
+            return minutes + " minutes";
+        }
+
+        Duration dur = Duration.ofMillis((long) _millis);
+        return String.format("%02d:%02d:%02d", dur.toHoursPart(), dur.toMinutesPart(), dur.toSecondsPart());
+    }
+
+    /**
+     * Returns the elapsed time in milliseconds formatted as string by calling {@link #getElapsedFormatted()}.
+     *
+     * @return formatted elapsed time
+     * @see #getElapsedFormatted()
      */
     @Override
     public String toString() {
-        if (tmf == null) {
-            return String.valueOf(getElapsed());
-        }
-        return tmf.format(getElapsed());
+        return getElapsedFormatted();
     }
 
 }
--- src/test/java/com/github/hypfvieh/util/TimeMeasureTest.java
+++ src/test/java/com/github/hypfvieh/util/TimeMeasureTest.java
@@ -1,45 +1,65 @@
 package com.github.hypfvieh.util;
 
-import org.junit.jupiter.api.Test;
-
 import com.github.hypfvieh.AbstractBaseUtilTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-public class TimeMeasureTest extends AbstractBaseUtilTest {
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+class TimeMeasureTest extends AbstractBaseUtilTest {
 
     @Test
-    public void testTimeMeasure() {
+    void testTimeMeasure() throws InterruptedException {
         TimeMeasure tm = new TimeMeasure();
-        assertTrue(tm.getElapsed() >= 0);
-        try {
-            Thread.sleep(100L);
-        } catch (InterruptedException _ex) {
-            assertTrue(true);
-        }
-        assertTrue(tm.getElapsed() >= 100);
-        assertTrue(tm.toString().matches("^[0-9]+ms$"), "toString() returned " + tm);
+        assertTrue(tm.getElapsedMillis() >= 0);
+        Thread.sleep(100L);
+        assertTrue(tm.getElapsedMillis() >= 100);
+        String str = tm.toString();
+        assertPatternMatches(str, "^[0-9\\.,]+ ms$");
 
         tm.reset();
-        long elapsed = tm.getElapsed();
+        long elapsed = tm.getElapsedMillis();
         assertTrue(elapsed < 10);
 
-        tm.setStartTm(tm.getStartTime() - 10000);
-        elapsed = tm.getElapsed();
-        assertTrue(elapsed >= 10000, "Elapsed was " + elapsed);
-        assertTrue(tm.toString().matches("^[0-9]+\\.[0-9]s$"), "toString() returned " + tm);
+        tm = new TimeMeasure(System.nanoTime() - TimeUnit.SECONDS.toNanos(10));
+        elapsed = tm.getElapsedMillis();
+        assertTrue(elapsed >= 10000);
+        assertPatternMatches(tm.toString(), "^[0-9\\.]+ seconds$");
     }
 
     @Test
-    public void testTimeMeasureFormatter() {
-        TimeMeasure tm = new TimeMeasure();
+    void testGetElapsed() {
+        long startNanos = System.nanoTime() - TimeUnit.SECONDS.toNanos(10);
+        TimeMeasure tm = new TimeMeasure(startNanos);
+        assertEquals(10, tm.getElapsedSeconds());
+    }
 
-        String oneSecond = tm.getElapsedFormatted(null, 1000);
-        assertEquals("00:00:01.000", oneSecond);
+    @Test
+    void testGetElapsedMinutes() {
+        long startNanos = System.nanoTime() - TimeUnit.MINUTES.toNanos(20) - TimeUnit.SECONDS.toNanos(15);
+        TimeMeasure tm = new TimeMeasure(startNanos);
+        assertEquals(20.25d, tm.getElapsedMinutes(), 0.00000001d);
+    }
 
-        String oneMinuteoneSecond = tm.getElapsedFormatted(null, 61000);
-        assertEquals("00:01:01.000", oneMinuteoneSecond);
-
-        String threeSecondsAfewMillis = tm.getElapsedFormatted(null, 3721);
-        assertEquals("00:00:03.721", threeSecondsAfewMillis);
-
+    @ParameterizedTest(name = "[{index}] {0} -> \"{1}\"")
+    @CsvSource(delimiterString = ";", value = {
+            "-1; '-1,00 ms'",
+            "0; 0,00 ms",
+            "0.165; 0,17 ms",
+            "0.5; 0,50 ms",
+            "1000; 1000 ms",
+            "1001; 1001 ms",
+            "3721; 3721 ms",
+            "4999; 4999 ms",
+            "5000; 5000 ms",
+            "5001; 5001 ms",
+            "61000; 61 seconds",
+            "180000; 180 seconds",
+            "3600000; 01:00:00"
+    })
+    void testFormatDuration(double _elapsed, String _expected) {
+        assertEquals(_expected, TimeMeasure.formatDuration(_elapsed, Locale.GERMAN));
     }
 }
