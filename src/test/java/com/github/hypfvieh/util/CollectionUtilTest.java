2c34d7f98810c4f5967af7b025bb34fa06725fd5
David M <maniac@case-of.org>
2025-04-21 14:23:58 +0200

    Added unit test

---
+++ src/test/java/com/github/hypfvieh/util/CollectionUtilTest.java
@@ -0,0 +1,34 @@
+package com.github.hypfvieh.util;
+
+import com.github.hypfvieh.AbstractBaseUtilTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+public class CollectionUtilTest extends AbstractBaseUtilTest {
+
+    @Test
+    void testHasValue() {
+        assertTrue(CollectionUtil.hasValue(List.of(1)));
+        assertTrue(CollectionUtil.hasValue(Set.of("yes")));
+
+        assertFalse(CollectionUtil.hasValue(List.of()));
+        assertFalse(CollectionUtil.hasValue(Set.of()));
+        assertFalse(CollectionUtil.hasValue(null));
+    }
+
+    @Test
+    void testCreateMutableList() {
+        List<Integer> mutableIntList = CollectionUtil.mutableListOf(1, 2, 3);
+
+        assertNotNull(mutableIntList);
+        assertEquals(3, mutableIntList.size());
+        assertEquals(List.of(1, 2, 3), mutableIntList);
+
+        assertDoesNotThrow(() -> mutableIntList.add(4));
+
+        assertEquals(4, mutableIntList.size());
+        assertEquals(List.of(1, 2, 3, 4), mutableIntList);
+    }
+}
